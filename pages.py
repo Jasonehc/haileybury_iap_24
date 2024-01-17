3e029c859446e0f336329cfe34bee27c327e5656
Jasonehc <94758745+Jasonehc@users.noreply.github.com>
2024-01-16 21:26:43 -0500

    Display query results

--- pages.py
+++ pages.py
@@ -5,12 +5,14 @@ import pymysql
 class App(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.geometry('600x640+0+0')
+        self.geometry('800x800+0+0')
         self.title('Personal Registration Form')
+
+        self.bg_color = '#3b5997'
 
         self.page_dictionary = {}
         self.page_dictionary["HomePage"] = HomePage(self)
-        self.page_dictionary["Page1"] = Page1(self)
+        self.page_dictionary["LookUp"] = LookUp(self)
         self.page_dictionary["RegistrationForm"] = RegistrationForm(self)
 
         self.raise_frame("HomePage")
@@ -22,37 +24,70 @@ class App(Tk):
 class HomePage(Frame):
     def __init__(self, root):
         Frame.__init__(self, root)
-        self.bg_color = '#3b5997'
-        title1 = Label(self, text = "Homepage", background = self.bg_color, foreground ="white")
-        title1.place(x=100,y=20,width=200,height=50)
+        self.root = root
+        title1 = Label(self, text = "User Registration Database", background = self.root.bg_color, foreground ="white")
+        title1.place(x=0,y=100,width=200,height=100)
 
-        button1 = Button(self, text = "Register new user", background = self.bg_color, foreground ="white", command = lambda: root.raise_frame("RegistrationForm"))
-        button1.place(x=100,y=195,width=200,height=25)
+        button1 = Button(self, text = "Register new user", background = self.root.bg_color, foreground ="white", command = lambda: root.raise_frame("RegistrationForm"))
+        button1.place(x=300,y=150,width=200,height=25)
 
-        button2 = Button(self, text = "Lookup users", background = self.bg_color, foreground ="white", command = lambda: root.raise_frame("Page1"))
-        button2.place(x=400,y=195,width=200,height=25)
+        button2 = Button(self, text = "Lookup users", background = self.root.bg_color, foreground ="white", command = lambda: root.raise_frame("LookUp"))
+        button2.place(x=300,y=175,width=200,height=25)
 
         self.place(x=100,y=20,width=1000,height=900)
 
-class Page1(Frame):
+class LookUp(Frame):
     def __init__(self, root):
         Frame.__init__(self, root)
-        self.bg_color = '#3b5997'
-        title1 = Label(self, text = "Page 1", background = self.bg_color, foreground ="white")
-        title1.place(x=100,y=20,width=200,height=50)
+        self.root = root
+        self.bg_color = self.root.bg_color
+        self.fg = 'white'
+        title = Label(self, text = "Query users", background = self.bg_color, foreground ="white")
+        title.place(x=0,y=20,width=200,height=50)
 
-        button1 = Button(self, text = "Go back", background = self.bg_color, foreground ="white", command = lambda: root.raise_frame("HomePage"))
-        button1.place(x=100,y=195,width=200,height=25)
+        button1 = Button(self, text = "Go back", background = self.bg_color, foreground ="white", command = self.return_to_home)
+        button1.place(x=0,y=150,width=100,height=25)
 
         self.place(x=100,y=20,width=1000,height=900)
+
+        query_label = Label(self, width=15, text = "Enter query:", background = self.bg_color, foreground='white')
+        query_label.place(x=0, y=100)
+
+        self.query_entry=Entry(self, width=50, borderwidth=2)
+        self.query_entry.place(x=120, y=100)
+
+        run_query_button = Button(self, text="Run query", width=15,borderwidth=5, height=2,bg=self.bg_color,fg=self.fg, cursor='hand2', border=2, command=self.run_query)
+        run_query_button.place(x=120, y=150, height=25)
+
+        self.query_results = Label(self, text = "")
+        self.query_results.place(x=0, y=200)
+        
+    def return_to_home(self):
+        self.query_results.config(text = "")
+        self.root.raise_frame("HomePage")
+
+    def run_query(self):
+        db = pymysql.connect(host='localhost', user='root', password='password', database='Personal_registration_form')
+        cur = db.cursor()
+        query = self.query_entry.get()
+        cur.execute(query)
+
+        counter = 0
+        txt = ""
+        for idx, row in enumerate(cur):
+            if idx > 10:
+                break
+            txt += str(row) + '\n'
+
+        self.query_results.config(text = txt)
 
 class RegistrationForm(Frame):
     def __init__(self, root, *args):
         Frame.__init__(self, root, *args)
         self.root = root
 
-        self.fg = '#0d4763'
-        self.bg = '#bed8e6'
+        self.fg = 'white'
+        self.bg = self.root.bg_color
 
         self.gender = StringVar(value="N/A")
         self.country = StringVar(value="N/A")
@@ -87,8 +122,8 @@ class RegistrationForm(Frame):
 
         # Commit the changes
         db.commit()
-
         messagebox.showinfo('Success', 'Registration successful!')
+        cur.close()
 
     def submit(self):
         if self.firstnameEntry.get()=='':
@@ -111,6 +146,7 @@ class RegistrationForm(Frame):
             messagebox.showerror('Alert! Passwords are not matching!')
         else:
             self.insert_data(self.firstnameEntry.get(), self.lastnameEntry.get(), self.emailEntry.get(), self.gender.get(), self.country.get(), self.usernameEntry.get(), self.passwordEntry.get())
+            self.go_back()
 
     def go_back(self):
         # Clear entry fields
@@ -118,7 +154,7 @@ class RegistrationForm(Frame):
         self.lastnameEntry.delete(0, 'end')
         self.emailEntry.delete(0, 'end')
         self.gender.set(0)  # Reset gender selection
-        self.country.set('Select Country')  # Reset country selection
+        self.country.set('Country:')  # Reset country selection
         self.usernameEntry.delete(0, 'end')
         self.passwordEntry.delete(0, 'end')
         self.confirmpasswordEntry.delete(0, 'end')
@@ -126,14 +162,15 @@ class RegistrationForm(Frame):
         self.displaypw2.set(0)  # Uncheck confirm password display toggle
 
     def place_buttons(self):
-        submit1btn = Button(self, text='Submit', width=15, borderwidth=5, height=2,bg=self.bg,fg=self.fg, cursor='hand2', border=2, command=self.submit)
-        submit1btn.place(x=150, y=500)
+        submit1btn = Button(self, text='submit', width=15, borderwidth=5, height=2,bg=self.bg,fg=self.fg, cursor='hand2', border=2, command=self.submit)
+        submit1btn.place(x=200, y=350)
 
         bckbtn = Button(self, text='reset', width=15, border=2, height=2,cursor='hand2', command=self.go_back)     
-        bckbtn.place(x=100, y=550)
+        bckbtn.place(x=100, y=350)
 
         home_button = Button(self, text='return to home', width=15, border=2, height=2,cursor='hand2', command= lambda: self.root.raise_frame("HomePage"))
-        home_button.place(x=0, y=550)
+        home_button.place(x=0, y=350)
+
     def place_checkboxes(self):
         def show1():
             if (self.displaypw1.get()==1):
@@ -147,71 +184,72 @@ class RegistrationForm(Frame):
             else:
                 self.confirmpasswordEntry.config(show='')
         
-        check1 = Checkbutton(self, variable= self.displaypw1, bg=self.bg,command=show1)
-        check1.place(x=420, y=350)
+        check1 = Checkbutton(self, variable= self.displaypw1,command=show1)
+        check1.place(x=425, y=257)
 
-        check2 = Checkbutton(self, variable= self.displaypw2, bg=self.bg,command=show2)
-        check2.place(x=420, y=400)
+        check2 = Checkbutton(self, variable= self.displaypw2,command=show2)
+        check2.place(x=425, y=287)
 
     def place_country_dropdown(self):
-        countryLabel=Label(self, text='Select Country:', fg=self.fg, bg=self.bg)
-        countryLabel.place(x=10, y=250)
+        countryLabel=Label(self, text='Country:', fg=self.fg, bg=self.bg, width=20)
+        countryLabel.place(x=0, y=200)
 
         countries=['Algeria', "Australia", 'Bahamas', 'Canada']
 
         countryLabelDropdown=OptionMenu(self, self.country, *countries)
-        countryLabelDropdown.place(x=240, y=250)
+        countryLabelDropdown.config(width=23)
+        countryLabelDropdown.place(x=240, y=195)
 
     def place_gender_buttons(self):
-        genderlbl =Label(self, text='Select Gender:', fg=self.fg, bg=self.bg)
-        genderlbl.place(x=10, y=200)
+        gender_label =Label(self, text='Gender:', fg=self.fg, bg=self.bg, width=20)
+        gender_label.place(x=0, y=170)
 
         genderRadio1=Radiobutton(self, text='Male', variable=self.gender, value='Male')
-        genderRadio1.place(x=240, y=200)
+        genderRadio1.place(x=240, y=170)
 
         genderRadio2=Radiobutton(self, text='Female', variable=self.gender, value='Female')
-        genderRadio2.place(x=350, y=200)
+        genderRadio2.place(x=350, y=170)
 
     def place_labels_and_entries(self):
-        firstname_label = Label(self, text="First Name:", fg=self.fg, bg=self.bg)
-        firstname_label.place(x=10, y=70)
+        firstname_label = Label(self, text="First Name:", fg=self.fg, bg=self.bg, width = 20)
+        firstname_label.place(x=0, y=80)
 
         self.firstnameEntry=Entry(self, width=30, borderwidth=2)
-        self.firstnameEntry.place(x=240, y=70)
+        self.firstnameEntry.place(x=240, y=80)
 
-        lastnamelbl = Label(self, text="Last Name:", fg=self.fg, bg=self.bg)
-        lastnamelbl.place(x=10, y=110)
+        lastnamelbl = Label(self, text="Last Name:", fg=self.fg, bg=self.bg, width = 20)
+        lastnamelbl.place(x=0, y=110)
 
         self.lastnameEntry=Entry(self, width=30, borderwidth=2)
         self.lastnameEntry.place(x=240, y=110)
 
-        emaillbl=Label(self, text='Email:', fg=self.fg, bg=self.bg)
-        emaillbl.place(x=10, y=150)
+        emaillbl=Label(self, text='Email:', fg=self.fg, bg=self.bg, width = 20)
+        emaillbl.place(x=0, y=140)
 
         self.emailEntry=Entry(self, width=30, borderwidth=2)
-        self.emailEntry.place(x=240, y=150)
+        self.emailEntry.place(x=240, y=140)
 
-        usernamelbl = Label(self, text='Username:', fg=self.fg, bg=self.bg)
-        usernamelbl.place(x=10, y=300)
+        usernamelbl = Label(self, text='Username:', fg=self.fg, bg=self.bg, width = 20)
+        usernamelbl.place(x=0, y=230)
 
         self.usernameEntry=Entry(self, width=30, borderwidth=2)
-        self.usernameEntry.place(x=240, y=300)
+        self.usernameEntry.place(x=240, y=230)
 
-        passwordlbl = Label(self, text='Password:', fg=self.fg, bg=self.bg)
-        passwordlbl.place(x=10, y=350)
+        passwordlbl = Label(self, text='Password:', fg=self.fg, bg=self.bg, width = 20)
+        passwordlbl.place(x=0, y=260)
 
         self.passwordEntry=Entry(self, width=30, borderwidth=2)
-        self.passwordEntry.place(x=240, y=350)
+        self.passwordEntry.place(x=240, y=260)
 
-        confirmpasswordlbl = Label(self, text='Confirm Password:',  fg=self.fg, bg=self.bg)
-        confirmpasswordlbl.place(x=10, y=400)
+        confirmpasswordlbl = Label(self, text='Confirm Password:',  fg=self.fg, bg=self.bg, width = 20)
+        confirmpasswordlbl.place(x=0, y=290)
 
         self.confirmpasswordEntry=Entry(self, width=30, borderwidth=2)
-        self.confirmpasswordEntry.place(x=240, y=400)
+        self.confirmpasswordEntry.place(x=240, y=290)
 
     def place_title(self):
         title = Label(self, text = "Personal Registration Form", background = self.bg, foreground = self.fg)
-        title.place(x=100,y=20,width=200,height=50)
+        title.place(x=0,y=20,width=425,height=50)
 
     def database_initialize(self):
         db = pymysql.connect(host='localhost', user = 'root', password=  'password')
