16924316dd9574fb99bf749d4c18854906c1262f
Jasonehc <94758745+Jasonehc@users.noreply.github.com>
2024-01-30 23:31:28 -0500

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,76 +1,87 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import pandas as pd
-from matplotlib.colors import ListedColormap
-from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 
-# Creation of the main perceptron object.
 class Perceptron(object):
-    #Initiating the learning rate and number of iterations.
-    def __init__(self, learning_rate=0.1, iterations=20):
+    def __init__(self, learning_rate, n_iter):
         self.learning_rate = learning_rate
-        self.iterations = iterations
-        self.weights = np.zeros(3)
-        self.end_weights = [[], [], []]
-    # Defining fit method for model training.
-    def train(self, x, y):
-        self.weights = np.zeros(1 + x.shape[1])
-        for i in range(self.iterations):
-            wrong_point = False
-            print("Starting iteration", i)
-            for xi, target in zip(x, y):
-                if target != self.predict(xi):
-                    wrong_point = True
+        self.n_iter = n_iter
+        self.weights = np.array([0.0, 0.0, 0.0])
+        self.end_weights = []
+
+    def fit(self, X, y):
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                print(xi, target)
+                predict = self.predict(xi)
+                if predict != target:
                     update = self.learning_rate * (target - self.predict(xi))
-                    self.weights[1:] += update*xi
+                    
+                    self.weights[1:] += update * xi
+                    
                     self.weights[0] += update
+                    errors += int(update != 0.0)
+                    self.end_weights.append((self.weights[0], self.weights[1], self.weights[2]))
+            self.errors_.append(errors)
+        return self
 
-                    self.end_weights[0].append(self.weights[0])
-                    self.end_weights[1].append(self.weights[1])
-                    self.end_weights[2].append(self.weights[2])
-            if not wrong_point:
-                break
-    
-    # Net Input method for summing the given matrix inputs and their corresponding weights.
-    def net_input(self, x):
-        return np.dot(x, self.weights[1:]) + self.weights[0]
+    def predict(self, X):
+        """Return class label after unit step"""
+        if np.dot(X, self.weights[1:]) + self.weights[0] > 0.0:
+            return 1
+        else:
+            return 0
 
-    # Predict method for predicting the classification of data inputs.
-    def predict(self, x):
-        return np.where(self.net_input(x) >= 0.0, 1, -1)
+# Obtain data
+iris = load_iris()
+X = iris.data
+y = iris.target
+data = np.hstack((X, y[:, np.newaxis]))
 
-# Data retrieval and preperation.
-y = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
-x = y.iloc[0:100, [0, 2]].values
+labels = iris.target_names
+features = iris.feature_names
+df = pd.DataFrame(data, columns=iris.feature_names+['label'])
+df.label = df.label.map({k:v for k,v in enumerate(labels)})
 
-y = y.iloc[0:100, 4].values
-y = np.where(y == 'Iris-setosa', -1, 1)
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'setosa', -1, 1)
 
-# Model training and evaluation.
-Classifier = Perceptron(learning_rate=0.01, iterations=50)
-Classifier.train(x, y)
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+#Training
+ppn = Perceptron(learning_rate=0.01, n_iter=10)
+
+ppn.fit(X, y)
 
 def visualize(points, line_weights):
-    n = len(line_weights[0])
+    line_weights = line_weights
     
     plt.xlim(4, 7)
     plt.ylim(0, 10)
 
-    for i in range(n):
+    for weight0, weight1, weight2 in line_weights:
         plt.clf()
-        plt.xlim(4, 7)
-        plt.ylim(0, 7)
+        plt.xlim(0, 10)
+        plt.ylim(0, 10)
         plt.scatter(points[:50, 0], points[:50, 1], color='red')
         plt.scatter(points[50:100, 0], points[50:100, 1], color='blue')
         
-        slope = -line_weights[2][i]/line_weights[1][i]
-        intercept = line_weights[0][i]
-        x_vals = np.linspace(4,7, 100)
+        slope = -weight1/weight2
+        intercept = weight0
+        print("line is y = " + str(slope) + "x+" + str(intercept))
+        x_vals = np.linspace(0,10, 100)
         y_vals = [slope*i+intercept for i in x_vals]
         plt.plot(x_vals, y_vals)
-        plt.pause(.5)
+        plt.pause(.1)
+    plt.pause(1)
 
-# Plotting
-end_weights = Classifier.end_weights
-print(end_weights)
-visualize(x, end_weights)
+print(ppn.weights)
+print(len(ppn.end_weights))
+
+visualize(X, ppn.end_weights)
