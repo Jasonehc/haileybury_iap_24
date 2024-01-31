9f3880e4527575f30340f23ca5b4ab1f182ac603
Jasonehc <94758745+Jasonehc@users.noreply.github.com>
2024-01-30 21:30:32 -0500

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -2,27 +2,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 from matplotlib.colors import ListedColormap
+from matplotlib.animation import FuncAnimation
 
 # Creation of the main perceptron object.
 class Perceptron(object):
     #Initiating the learning rate and number of iterations.
-    def __init__(self, learning_rate=0.5, iterations=10):
+    def __init__(self, learning_rate=0.1, iterations=20):
         self.learning_rate = learning_rate
         self.iterations = iterations
         self.weights = np.zeros(3)
-    
+        self.end_weights = [[], [], []]
     # Defining fit method for model training.
     def train(self, x, y):
         self.weights = np.zeros(1 + x.shape[1])
         for i in range(self.iterations):
+            wrong_point = False
             print("Starting iteration", i)
             for xi, target in zip(x, y):
-                update = self.learning_rate * (target - self.predict(xi))
-                self.weights[1:] += update*xi
-                self.weights[0] += update
-            print(self.weights)
-            #self.plot_decision_line(x, y)
-        return self
+                if target != self.predict(xi):
+                    wrong_point = True
+                    update = self.learning_rate * (target - self.predict(xi))
+                    self.weights[1:] += update*xi
+                    self.weights[0] += update
+
+                    self.end_weights[0].append(self.weights[0])
+                    self.end_weights[1].append(self.weights[1])
+                    self.end_weights[2].append(self.weights[2])
+            if not wrong_point:
+                break
     
     # Net Input method for summing the given matrix inputs and their corresponding weights.
     def net_input(self, x):
@@ -31,23 +38,39 @@ class Perceptron(object):
     # Predict method for predicting the classification of data inputs.
     def predict(self, x):
         return np.where(self.net_input(x) >= 0.0, 1, -1)
-    
-    def plot_decision_line(self, x, y):
-        # setup marker generator and color map
-        slope = -self.weights[2]/self.weights[1]
-        intercept = self.weights[0]
 
 # Data retrieval and preperation.
 y = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
-print(y)
 x = y.iloc[0:100, [0, 2]].values
-print(x)
-plt.scatter(x[:50, 0], x[:50, 1], color='red')
-plt.scatter(x[50:100, 0], x[50:100, 1], color='blue')
-plt.show()
+
 y = y.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 
 # Model training and evaluation.
 Classifier = Perceptron(learning_rate=0.01, iterations=50)
 Classifier.train(x, y)
+
+def visualize(points, line_weights):
+    n = len(line_weights[0])
+    
+    plt.xlim(4, 7)
+    plt.ylim(0, 10)
+
+    for i in range(n):
+        plt.clf()
+        plt.xlim(4, 7)
+        plt.ylim(0, 7)
+        plt.scatter(points[:50, 0], points[:50, 1], color='red')
+        plt.scatter(points[50:100, 0], points[50:100, 1], color='blue')
+        
+        slope = -line_weights[2][i]/line_weights[1][i]
+        intercept = line_weights[0][i]
+        x_vals = np.linspace(4,7, 100)
+        y_vals = [slope*i+intercept for i in x_vals]
+        plt.plot(x_vals, y_vals)
+        plt.pause(.5)
+
+# Plotting
+end_weights = Classifier.end_weights
+print(end_weights)
+visualize(x, end_weights)
